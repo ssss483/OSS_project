229c7c0167dd6f13c55f43e14711dcdef3598aa9
hyunji999 <lizzie1019@naver.com>
2025-12-06 21:18:17 +0900

    Update and rename fill_mask.py to module5.py

--- module5.py
+++ module5.py
@@ -1,4 +1,7 @@
 # module 5 
+# replace <mask> to tokens with KLUE RoBERTa predictions.
+
+
   def apply_fill_mask(masked_sentence):
     predictions = fill_mask(masked_sentence)
     best = predictions[0]["token_str"].strip() # Extracts the top predicted token and removes extra whitespace.
